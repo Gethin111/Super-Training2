9f48d484dc5411765ea6226d4022ed0088518a52
Gethin111 <103399867+Gethin111@users.noreply.github.com>
2022-04-27 17:29:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateform(){  
+    var aname=document.myform.aname.value;  
+    var Email=document.myform.Email.value;  
+    var message=document.myform.message.value;  
+    var phone=document.myform.phone.value;   
+    if (aname==null || aname==""||Email==null || Email==""||message==null || message==""||phone==null || phone=="")
+    {  
+      alert("Information cannot be empty");  
+      return false;  
+    }
+}
+//Gethin(Wu Guoxiang)The main function of this function is to let the user to enter text and numbers, when there is no input or text and number input error display: the information can not be empty. When the input is correct, it will show the login success, which is the function of this script, widely used in the login interface of the input.
